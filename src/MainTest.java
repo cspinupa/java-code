5e7815406d52cbdde8c53df973132e63c6c11199
CS <chetan.pinupa@gmail.com>
2021-05-02 22:55:37 +0530

    Added placeholder pacakges for other practices

--- src/MainTest.java
+++ src/MainTest.java
@@ -1,9 +1,16 @@
+import com.java.java11.Main11;
 import com.java.java8.Main8;
 
 class MainTest {
     public static void main(String[] args) {
-        System.out.println("***** Java 8 features ****");
+        System.out.println(" \n ***** Java 8 features start ****");
         Main8 main = new Main8();
         main.parseArr();
+        System.out.println(" \n ***** Java 8 features end ****");
+
+        System.out.println(" \n ***** Java 11 features start ****");
+        Main11 main11 = new Main11();
+        System.out.println(" \n ***** Java 11 features end ****");
+
     }
 } 
--- src/com/java/java8/Main8.java
+++ src/com/java/java8/Main8.java
@@ -19,10 +19,11 @@ public class Main8 {
 
         System.out.println(" \n Sum of values in the array ->"+Arrays.stream(arr).sum());
 
-        System.out.println(" \n Values which are less than 10 in the array ->");
+        System.out.print(" \n Values which are less than 10 in the array ->");
         Arrays.stream(arr)
         .filter(num -> num < 10)
         .forEach( i -> System.out.print(" "+i));
+        System.out.println("");
         
     }
 }
